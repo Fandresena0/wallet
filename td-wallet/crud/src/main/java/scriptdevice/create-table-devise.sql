52543dd271110fbddd0675a55ee8422e28f8ec31
Fandresena0 <hei.fandresena.2@gmail.com>
2023-12-08 15:13:49 +0300

    mise en place de la base de donné

--- td-wallet/crud/src/main/java/scriptdevice/create-table-devise.sql
+++ td-wallet/crud/src/main/java/scriptdevice/create-table-devise.sql
@@ -1,1 +1,12 @@
-CREATE TYPE IF NOT EXISTS devise AS ENUM ('ARIARY', 'EURO', 'DOLAR');
+CREATE TYPE IF NOT EXISTS devise_nom AS ENUM ('Euro', 'Ariary');
+
+CREATE TABLE IF NOT EXISTS devise (
+        id_devise serial PRIMARY KEY,
+        nom_devise devise_nom,
+        code_devise varchar(3) CHECK (code_devise IN ('EUR', 'MGA'))
+    );
+
+INSERT INTO devise (nom_devise, code_devise)
+VALUES
+    ('Euro', 'EUR'),
+    ('Ariary', 'MGA');
