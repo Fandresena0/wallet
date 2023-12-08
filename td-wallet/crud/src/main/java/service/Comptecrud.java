552171e7f5c01f971c681b0b6a5c1d7ddc98d8d7
Fandresena0 <hei.fandresena.2@gmail.com>
2023-12-08 14:58:35 +0300

    resolution de l'erreur du crud

--- td-wallet/crud/src/main/java/service/Comptecrud.java
+++ td-wallet/crud/src/main/java/service/Comptecrud.java
@@ -13,18 +13,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @AllArgsConstructor
-public class Comptecrud implements crudOperation{
+public class Comptecrud implements crudOperation {
+
     private Connection connection;
+
     @Override
     public Compte insert(Compte toInsert) {
-        String query = "INSERT INTO compte (id_compte, nome_utilisateur, transaction,devisse) VALUES (?, ?, ?,?)";
+        String query = "INSERT INTO compte (id_compte, nom, solde, devise, type) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, toInsert.getId_compte());
-            preparedStatement.setString(2, toInsert.getNom_utilisateur());
-            preparedStatement.setString(3, String.valueOf(toInsert.getTransaction()));
-            preparedStatement.setString(4,String.valueOf(toInsert.getDevise()));
+            preparedStatement.setString(2, toInsert.getNom());
+            preparedStatement.setInt(3, toInsert.getSolde());
+            preparedStatement.setString(4, toInsert.getDevise().getNom()); // Modifie selon la structure de ta base de données
+            preparedStatement.setString(5, toInsert.getType());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -35,27 +37,30 @@ public class Comptecrud implements crudOperation{
     @Override
     public List<Compte> findAll() throws SQLException {
         List<Compte> allCompte = new ArrayList<>();
-        String sql="SELECT * FROM compte";
-        try (PreparedStatement preparedStatement=connection.prepareStatement(sql)){
+        String sql = "SELECT * FROM compte";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             ResultSet result = preparedStatement.executeQuery();
-            convertToList(allCompte,result);
+            while (result.next()) {
+                allCompte.add(convertToCompte(result));
+            }
         }
         return allCompte;
     }
 
-    private void convertToList(List<Compte> allCompte, ResultSet result) throws SQLException{
-       allCompte.add(new Compte(
-               result.getInt("id_compte"),
-               result.getString("nome_utilisateur"),
-               (Transaction) result.getObject("transaction"),
-               (Devise) result.getObject("devise")
-       ));
+    private Compte convertToCompte(ResultSet result) throws SQLException {
+        int id_compte = result.getInt("id_compte");
+        String nom = result.getString("nom");
+        Integer solde = result.getInt("solde");
+        Devise devise = new Devise(result.getInt("id_devise"), result.getString("nom_devise"), result.getString("code_devise"));
+        String type = result.getString("type");
+
+        return new Compte(id_compte, nom, solde, devise, type);
     }
 
     @Override
     public Compte delete(Compte toDelete) throws SQLException {
         String sql = "DELETE FROM compte WHERE id_compte = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, toDelete.getId_compte());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -64,6 +69,4 @@ public class Comptecrud implements crudOperation{
         System.out.println("DELETE 01");
         return toDelete;
     }
-
-
 }
